79be85e180bb1d23eaa054296bf96eb2fd155bc1
alHasandev <alhasan99dev@gmail.com>
2023-03-22 10:56:04 +0800

    feat: setup basic error handling

--- src/app/api/basic/raw/route.ts
+++ src/app/api/basic/raw/route.ts
@@ -3,13 +3,46 @@ import loader from '../loader';
 import { RequestData } from '../route';
 
 export async function POST(request: Request) {
-  const res = await request.json();
-  const parsed = RequestData.safeParse(res);
+  try {
+    const res = await request.json();
+    const parsed = RequestData.safeParse(res);
 
-  if (!parsed.success) return NextResponse.json(parsed.error.format());
+    if (!parsed.success) return NextResponse.json(parsed.error.format());
 
-  const reqData = parsed.data;
+    const reqData = parsed.data;
 
-  const dataset = loader(reqData.dataset, reqData.schema);
-  return NextResponse.json(dataset);
+    const dataset = loader(reqData.dataset, reqData.schema);
+    return NextResponse.json(dataset);
+  } catch (error) {
+    const err = error as Error;
+
+    if (err.name === 'SyntaxError') {
+      return NextResponse.json(
+        {
+          error: {
+            code: 422,
+            name: 'UnprocessableContent',
+            messsage: 'Your request data is invalid',
+          },
+        },
+        {
+          status: 422,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        error: {
+          code: 500,
+          name: err.name,
+          messsage: err.message,
+          cause: err.cause,
+        },
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
--- src/app/api/basic/route.ts
+++ src/app/api/basic/route.ts
@@ -8,16 +8,49 @@ export const RequestData = z.object({
 });
 
 export async function POST(request: Request) {
-  const res = await request.json();
-  const parsed = RequestData.safeParse(res);
+  try {
+    const res = await request.json();
+    const parsed = RequestData.safeParse(res);
 
-  if (!parsed.success) return NextResponse.json(parsed.error.format());
+    if (!parsed.success) return NextResponse.json(parsed.error.format());
 
-  const reqData = parsed.data;
+    const reqData = parsed.data;
 
-  const dataset = loader(reqData.dataset, reqData.schema);
-  return NextResponse.json({
-    schema: reqData.schema,
-    dataset: dataset,
-  });
+    const dataset = loader(reqData.dataset, reqData.schema);
+    return NextResponse.json({
+      schema: reqData.schema,
+      dataset: dataset,
+    });
+  } catch (error) {
+    const err = error as Error;
+
+    if (err.name === 'SyntaxError') {
+      return NextResponse.json(
+        {
+          error: {
+            code: 422,
+            name: 'UnprocessableContent',
+            messsage: 'Your request data is invalid',
+          },
+        },
+        {
+          status: 422,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        error: {
+          code: 500,
+          name: err.name,
+          messsage: err.message,
+          cause: err.cause,
+        },
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
